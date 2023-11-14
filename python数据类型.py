4fff0e12f1cbf55efc337cf9fcc5ac0d8c04d9c9
Kevin <568832023@qq.com>
2023-11-14 22:23:42 +0800

    更新出版python数据类型代码

---
+++ python数据类型.py
@@ -0,0 +1,101 @@
+# python
+# num_a = 10
+# num_b = 4
+# print(type(num_a))
+# print(type(num_a / num_b))  # 知识点1: python中所有的变量都会进行自动的转型处理
+
+'''
+知识点2：None是无法确定类型的
+知识点3:python中提供了type()函数用于方便的获取数据的类型
+'''
+# num = None
+# print(type(num))
+
+# num_a = 10E5
+# num_b = 30.3E6
+# print(num_a * num_b)
+# 知识点4: python3中取消了对于数据的保存长度的限制，理论上可以存很大的数据
+
+# num = complex(10, 2)
+# print(num)
+# print(num.real)  # 复数的实部
+# print(num.imag)  # 复数的虚部
+# print(num.conjugate())  # 共轭复数
+
+# flag = True
+# if flag:
+#     print("Hello World!")
+# 知识点5:非零的数字都是True，但是习惯而言，用0代表False，1代表True
+
+# 知识点6:python中没有字符的概念
+
+# info = "Hello"
+# message = 'World!'
+# print(type(info))  # <class 'str'>
+# print(info)
+# print(message)
+# 知识点7: python中的单双引号和java一样可以嵌套
+
+# 观察python中的三种引号
+# info = """
+#         message : "Hello World"
+#         name : 'Kevin !!!'
+#     """
+# print(info)
+
+# 再来观察一下续航符
+# info = "Hello" \
+#     "'World'" \
+#     "Kevin"
+# print(info)
+
+# 定义字符串连接
+# info = "魔乐科技 ： " + "www.mldn.cn"
+# info += ",Kevin"
+# print(info)
+
+# 使用转义字符
+# info = "魔乐科技 ： \"www.mldn.cn\" \n\t 极限IT:'www.jixianit.com'"
+# print(info)
+
+# 键盘输入
+# input_data = input("请输入信息 ： ")
+# print("输入信息为：" + input_data)
+# 知识点8 ： 所有数据的类型全部是字符，如果需要接受其他的类型，需要使用转换函数
+
+# 知识点9 ： 转换函数：int() , float() , bool() , str()
+# str = "168"
+# temp = int(str)
+# print(type(temp))
+# print(temp)
+
+# 格式化输出
+# age = 18
+# name = "Kevin"
+# salary = 49999.5123213123
+# str = "%s 今年 %d ,工资 %f" % (name, age, salary)
+# print(str)
+# print("%s 今年 %d ,工资 %5.2f" % (name, age, salary))  # 设置格式化辅助标记，显示两位小数
+# print("%(name)s 今年 %(age)d ,工资 %(salary)5.2f" % vars())  # 通过vars()函数获取所有的变量
+# 知识点10 ： 在默认情况下使用print()函数输出数据时，都会使用换行作为分隔符
+# 自定义完结符
+# print("Wow Kevin is really", end="!!")
+# print("handsome", end="\n")
+# print("hahahaha...", end="--------")
+
+80%
+
+高级算法开发工程师 3000
+初级算法开发工程师 2000
+3000 * 80% = 2400
+2000 * 80% = 1600
+35000
+52000
+
+以项目实际完成验收的时间为准进行结算。
+
+
+
+
+
+
